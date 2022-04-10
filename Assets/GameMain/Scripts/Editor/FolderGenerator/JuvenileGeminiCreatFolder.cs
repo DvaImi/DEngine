4814c9016c88dd4fcd9f2fee6baf120a21501158
Juvenile <2290810572@qq.com>
2022-04-10 16:07:42 +0800

    导入框架

---
+++ Assets/GameMain/Scripts/Editor/FolderGenerator/JuvenileGeminiCreatFolder.cs
@@ -0,0 +1,70 @@
+// ========================================================
+// 描述：
+// 作者：JuvenileGemini 
+// 创建时间：2022-04-10 10:12:12
+// 版 本：1.0
+// ========================================================
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+namespace JuvenileGemini.Editor
+{
+    public class JuvenileGeminiCreatFolder
+    {
+        const string gameMainfolder = "Assets/GameMain/";
+        const string gameEntitiesFolder = gameMainfolder + "Entities";
+        const string gameMaterialsFolder = gameMainfolder + "Materials";
+        const string gameMeshsFolder = gameMainfolder + "Meshs";
+        const string gameMusicFolder = gameMainfolder + "Music";
+        const string gameScenesFolder = gameMainfolder + "Scenes";
+        const string gameSoundsFolder = gameMainfolder + "Sounds";
+        const string gameShadersFolder = gameMainfolder + "Shaders";
+        const string gameTexturesFolder = gameMainfolder + "Textures";
+
+
+        [MenuItem("JuvenileGemini/创建文件夹/统一生成必要文件夹")]
+        public static void CreatAllFolder()
+        {
+            if (!Directory.Exists(gameEntitiesFolder))
+            {
+                Directory.CreateDirectory(gameEntitiesFolder);
+            }
+            if (!Directory.Exists(gameMaterialsFolder))
+            {
+                Directory.CreateDirectory(gameMaterialsFolder);
+            }
+            if (!Directory.Exists(gameMeshsFolder))
+            {
+                Directory.CreateDirectory(gameMeshsFolder);
+            }
+            if (!Directory.Exists(gameMusicFolder))
+            {
+                Directory.CreateDirectory(gameMusicFolder);
+            }
+            if (!Directory.Exists(gameScenesFolder))
+            {
+                Directory.CreateDirectory(gameScenesFolder);
+            }
+            if (!Directory.Exists(gameSoundsFolder))
+            {
+                Directory.CreateDirectory(gameSoundsFolder);
+            }
+            if (!Directory.Exists(gameShadersFolder))
+            {
+                Directory.CreateDirectory(gameShadersFolder);
+            }
+            if (!Directory.Exists(gameTexturesFolder))
+            {
+                Directory.CreateDirectory(gameTexturesFolder);
+            }
+            if (!Directory.Exists(Application.streamingAssetsPath))
+            {
+                Directory.CreateDirectory(Application.streamingAssetsPath);
+            }
+            AssetDatabase.Refresh();
+        }
+    }
+}
+
+
---
+++ Assets/GameMain/Scripts/Procedure/ProcedureMain.cs
@@ -0,0 +1,47 @@
+﻿//------------------------------------------------------------
+// Game Framework
+// Copyright © 2013-2021 Jiang Yin. All rights reserved.
+// Homepage: https://gameframework.cn/
+// Feedback: mailto:ellan@gameframework.cn
+//------------------------------------------------------------
+
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace JuvenileGemini
+{
+    public class ProcedureMain : ProcedureBase
+    {
+        public override bool UseNativeDialog
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+        }
+
+        protected override void OnDestroy(ProcedureOwner procedureOwner)
+        {
+            base.OnDestroy(procedureOwner);
+        }
+
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+        }
+
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+        }
+    }
+}
