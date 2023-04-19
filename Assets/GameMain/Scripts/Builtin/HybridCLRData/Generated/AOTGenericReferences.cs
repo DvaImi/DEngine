75bdb4a71a18efece39614f8a0a358d26a608c7c
稚痕 <1226716976@qq.com>
2023-04-19 19:31:32 +0800

    更新忽略文件，添加热更新启动器

---
+++ Assets/GameMain/Scripts/Builtin/HybridCLRData/Generated/AOTGenericReferences.cs
@@ -0,0 +1,58 @@
+public class AOTGenericReferences : UnityEngine.MonoBehaviour
+{
+
+	// {{ AOT assemblies
+	// GameFramework.dll
+	// UnityEngine.CoreModule.dll
+	// UnityGameFramework.Runtime.dll
+	// mscorlib.dll
+	// }}
+
+	// {{ constraint implement type
+	// }} 
+
+	// {{ AOT generic types
+	// GameFramework.DataTable.IDataTable<object>
+	// GameFramework.Fsm.FsmState<object>
+	// GameFramework.Fsm.IFsm<object>
+	// System.Collections.Generic.Dictionary<object,byte>
+	// System.Collections.Generic.Dictionary.Enumerator<object,byte>
+	// System.Collections.Generic.IEnumerator<object>
+	// System.Collections.Generic.KeyValuePair<object,byte>
+	// System.Collections.Generic.List<object>
+	// System.Collections.Generic.List.Enumerator<object>
+	// System.EventHandler<object>
+	// System.Nullable<int>
+	// System.Runtime.CompilerServices.AsyncTaskMethodBuilder<object>
+	// System.Runtime.CompilerServices.TaskAwaiter<object>
+	// System.Threading.Tasks.Task<object>
+	// System.Threading.Tasks.TaskCompletionSource<object>
+	// System.Threading.Tasks.TaskCompletionSource<byte>
+	// }}
+
+	public void RefMethods()
+	{
+		// System.Void GameFramework.Fsm.FsmState<object>.ChangeState<object>(GameFramework.Fsm.IFsm<object>)
+		// object GameFramework.Fsm.IFsm<object>.GetData<object>(string)
+		// System.Void GameFramework.Fsm.IFsm<object>.SetData<object>(string,object)
+		// object GameFramework.GameFrameworkEntry.GetModule<object>()
+		// System.Void GameFramework.Procedure.IProcedureManager.StartProcedure<object>()
+		// string GameFramework.Utility.Text.Format<object,object>(string,object,object)
+		// string GameFramework.Utility.Text.Format<object>(string,object)
+		// string GameFramework.Utility.Text.Format<GameFramework.Localization.Language,object,object>(string,GameFramework.Localization.Language,object,object)
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder<object>.AwaitUnsafeOnCompleted<System.Runtime.CompilerServices.TaskAwaiter<object>,Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAssets>d__23>(System.Runtime.CompilerServices.TaskAwaiter<object>&,Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAssets>d__23&)
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder<object>.Start<Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAsset>d__24<object>>(Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAsset>d__24<object>&)
+		// System.Void System.Runtime.CompilerServices.AsyncTaskMethodBuilder<object>.Start<Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAssets>d__23>(Dvalmi.Hotfix.AwaitUtility.<AwaitLoadAssets>d__23&)
+		// object UnityEngine.Component.GetComponent<object>()
+		// System.Void UnityEngine.Component.GetComponentsInChildren<object>(bool,System.Collections.Generic.List<object>)
+		// object UnityExtension.GetOrAddComponent<object>(UnityEngine.GameObject)
+		// GameFramework.DataTable.IDataTable<object> UnityGameFramework.Runtime.DataTableComponent.GetDataTable<object>()
+		// bool UnityGameFramework.Runtime.FsmComponent.DestroyFsm<object>()
+		// System.Void UnityGameFramework.Runtime.Log.Error<object,object,object>(string,object,object,object)
+		// System.Void UnityGameFramework.Runtime.Log.Error<object,object>(string,object,object)
+		// System.Void UnityGameFramework.Runtime.Log.Info<object,object,object,object>(string,object,object,object,object)
+		// System.Void UnityGameFramework.Runtime.Log.Info<object>(string,object)
+		// System.Void UnityGameFramework.Runtime.Log.Info<object,object>(string,object,object)
+		// System.Void UnityGameFramework.Runtime.Log.Warning<object>(string,object)
+	}
+}
