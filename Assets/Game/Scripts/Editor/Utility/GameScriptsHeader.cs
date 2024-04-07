e2877115556a8e1c259d506c78a2d5a19185e7a3
Dvalmi <1226716976@qq.com>
2024-04-08 00:58:09 +0800

    添加自动添加头部描述脚本

---
+++ Assets/Game/Scripts/Editor/Utility/GameScriptsHeader.cs
@@ -0,0 +1,31 @@
+using System.IO;
+
+namespace Game.Editor
+{
+    /// <summary>
+    /// 脚本头文件
+    /// </summary>
+    public class GameScriptsHeader : UnityEditor.AssetModificationProcessor
+    {
+        /// <summary>
+        /// 在资源创建时调用
+        /// </summary>
+        /// <param name="path">自动传入资源路径</param>
+        public static void OnWillCreateAsset(string path)
+        {
+            path = path.Replace(".meta", "");
+            if (!path.EndsWith(".cs"))
+            {
+                return;
+            }
+
+            string allText = "// ========================================================\r\n"
+                             + "// 作者：Dvalmi \r\n"
+                             + "// 创建时间：#CreateTime#\r\n"
+                             + "// ========================================================\r\n";
+            allText += File.ReadAllText(path);
+            allText = allText.Replace("#CreateTime#", System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+            File.WriteAllText(path, allText);
+        }
+    }
+}
