39746ec57a36e4829af72b4f4a9980509843be73
稚痕 <1226716976@qq.com>
2024-09-16 03:58:59 +0800

    luban配置上传

---
+++ Share/Luban/gen_bin__unitask.bat
@@ -0,0 +1,17 @@
+cd /d %~dp0
+set WORKSPACE=.
+
+set GEN_CLIENT=%WORKSPACE%\Tools\Luban.dll
+set CONF_ROOT=%WORKSPACE%\Configs
+set CODE_UNITASK=%CONF_ROOT%\CustomTemplate_Client_UniTask
+
+dotnet %GEN_CLIENT% ^
+    -t client ^
+    -c cs-bin ^
+    -d bin  ^
+    --conf %CONF_ROOT%\luban.conf ^
+    --customTemplateDir %CODE_UNITASK% ^
+    -x outputCodeDir=../../Client/DEngine/Assets/Game/Scripts/Update/Generate/GameConfig ^
+    -x outputDataDir=../../Client/DEngine/Assets/Game/Bundles/Configs/Bin/
+pause
+    
---
+++ Share/Luban/gen_bin__unitask.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 设置工作目录
+WORKSPACE=.
+
+# 设置生成客户端的工具和配置文件路径
+GEN_CLIENT="$WORKSPACE/Tools/Luban.dll"
+CONF_ROOT="$WORKSPACE/Configs"
+CODE_UNITASK="$CONF_ROOT/CustomTemplate_Client_UniTask"
+
+# 使用 dotnet 执行 Luban 的生成命令
+dotnet "$GEN_CLIENT" \
+    -t client \
+    -c cs-bin \
+    -d bin \
+    --conf "$CONF_ROOT/luban.conf" \
+    --customTemplateDir "$CODE_UNITASK" \
+    -x outputCodeDir=../../Client/DEngine/Assets/Game/Scripts/Update/Generate/GameConfig \
+    -x outputDataDir=../../Client/DEngine/Assets/Game/Bundles/Configs/Bin/
+
+# 暂停（可选）
+read -p "Press any key to continue..."
