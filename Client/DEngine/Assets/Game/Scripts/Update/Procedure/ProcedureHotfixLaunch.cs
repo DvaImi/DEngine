96ce543455ce983fe0219f22309eab769ed6b3fe
Dvalmi <1226716976@qq.com>
2024-09-15 11:00:12 +0800

    调整流程

--- Client/DEngine/Assets/Game/Scripts/Update/Procedure/ProcedureHotfixLaunch.cs
+++ Client/DEngine/Assets/Game/Scripts/Update/Procedure/ProcedureHotfixLaunch.cs
@@ -7,47 +7,17 @@ namespace Game.Update
 {
     public class ProcedureHotfixLaunch : ProcedureBase
     {
-        private bool m_InitializeComplete;
-
-        /// <summary>
-        /// 热更新流程启动
-        /// </summary>
-        /// <param name="procedureOwner"></param>
-        protected override void OnInit(ProcedureOwner procedureOwner)
-        {
-            base.OnInit(procedureOwner);
-        }
-
-        protected override void OnEnter(ProcedureOwner procedureOwner)
+        protected override async void OnEnter(ProcedureOwner procedureOwner)
         {
             base.OnEnter(procedureOwner);
             Log.Info("ProcedureHotfix  Launch  ");
-            m_InitializeComplete = false;
-            InitializeArchive().Forget();
-            InitializeNetwork().Forget();
-        }
-
-
-        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
-        {
-            base.OnLeave(procedureOwner, isShutdown);
-        }
-
-        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
-        {
-            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
-            if (!m_InitializeComplete)
-            {
-                return;
-            }
-
+            await UniTask.WhenAll(InitializeArchive(), InitializeNetwork());
             ChangeState<ProcedurePreload>(procedureOwner);
         }
 
         private async UniTask InitializeArchive()
         {
             await GameEntry.Archive.Initialize();
-            m_InitializeComplete = true;
             Log.Info("Init Archive complete.");
         }
 
