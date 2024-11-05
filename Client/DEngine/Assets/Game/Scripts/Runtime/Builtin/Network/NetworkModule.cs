7206beac4c584333e104772adfda17a812572340
DvaImi <2290810572@qq.com>
2024-11-05 22:12:35 +0800

    添加变量池

--- Client/DEngine/Assets/Game/Scripts/Runtime/Builtin/Network/NetworkModule.cs
+++ Client/DEngine/Assets/Game/Scripts/Runtime/Builtin/Network/NetworkModule.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DEngine;
 using Fantasy.Network;
-using Fantasy.Platform.Unity;
-using Log = Fantasy.Log;
 
 namespace Game.Network
 {
